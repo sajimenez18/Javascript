9b8597cf8714096e22e498a675ee18b19a67b011
Stephs <sajimenez@scl.edu.gt>
2025-08-27 18:02:35 -0600

    Nuevo contenido

---
+++ estructuraDeControlIf.js
@@ -0,0 +1,14 @@
+let num21 = 10;
+let num32 = 100;
+
+//If normal 
+if(num21!= num32){
+    num21 = 0;
+}
+else {
+    num21 = 1;
+}
+/*If opcion corta
+num21 = num21 != num32 ? num21 = 0: num21 = 1
+*/
+console.log(num21);
